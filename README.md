99d3bacc19bb6530f1c4be26b368d05cf6a73d0a
Ben <nimajnebs@gmail.com>
2013-08-10 22:33:30 -0700

    Update the app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Landing Page
 
 A Simple HTML/JS Landing/Construction Page
+
+This will get folded into BenPress as an included theme.
--- assets/js/init.js
+++ assets/js/init.js
@@ -8,7 +8,7 @@ var nbOfTicks = 0;
 var videoDefined = false;
 	
 $(document).ready(function() {
-
+                
 	_startLoading();
 
 	// fade in backgrounds, countdown
@@ -78,19 +78,25 @@ $(document).ready(function() {
 	
 	
 	// init countdown
-	if($("#countdown-widget").data('time') != '') {
-		launchTime = new Date(Date.parse($("#countdown-widget").data('time')));
+	var countdown_time = $("#countdown-widget").data('time');
+	var countdown_timezone = $("#countdown-widget").data('timezone');
+	
+	if(countdown_time != '') {
+		launchTime = new Date(Date.parse(countdown_time));
 	}else{
 		launchTime = new Date(); // Set launch: [year], [month], [day], [hour]...
 		launchTime.setDate(launchTime.getDate() + 15); // Add 15 days
-	}	
+	}
+	if(countdown_timezone == '')
+		countdown_timezone = null;
+			
 	$("#countdown-widget").countdown({
 		until: launchTime, 
 		format: "dHMS",
 		labels: ['','','','','','',''],
 		digits:['0','1','2','3','4','5','6','7','8','9'],
+		timezone: countdown_timezone,
 		onTick: _onTick
-		
 	});
 	
 
