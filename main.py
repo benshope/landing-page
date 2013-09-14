9ecf3e8a639111d1a278bef05e13dd83429ba728
Ben <nimajnebs@gmail.com>
2013-09-14 01:42:48 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from google.appengine.api import mail
 
 class SendMail(webapp2.RequestHandler):
   def post(self):
-    mail.send_mail(sender='{0} <{1}>'.format(self.request.get('name'), self.request.get('email')),
-                  to="Senior Spring Design <info@seniorspring.com>",
+    mail.send_mail(sender="{0} <{1}>".format(self.request.get('name'), self.request.get('email')),
+                  to="Senior Spring Design <nimajnebs@gmail.com>",
                   subject="Senior Spring Contact Form Message",
                   body=self.request.get('message'))
     
