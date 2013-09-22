8e15d84cb9d012c7c1b03c56df659f5d53ab8aad
Ben <nimajnebs@gmail.com>
2013-09-21 23:11:32 -0700

    Auto-Update

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import cgi
 import urllib
 from google.appengine.api import mail
 
-
 class SendMail(webapp2.RequestHandler):
   def post(self):
     mail.send_mail(sender="{0} <{1}>".format(self.request.get('name'), self.request.get('email')),
@@ -13,7 +12,6 @@ class SendMail(webapp2.RequestHandler):
                   subject="Senior Spring Contact Form Message",
                   body=self.request.get('message'))
     
-
 app = webapp2.WSGIApplication([
   ('/mail', SendMail)
 ])
